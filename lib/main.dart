cf2dc7d855c710467891d8f2de962a9e234bac25
casey cole <casey@capsinc.com>
2021-04-17 18:11:55 -0400

    Basic Scaffoling and press hooked up

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: TextButton(
+          child: Image.asset('images/ball$num.png'),
+          onPressed: () {
+            print(num);
+          },
+        ),
+      ),
+    );
+  }
+}
